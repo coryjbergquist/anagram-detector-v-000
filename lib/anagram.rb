a1653931c803bc6ee0f0b124695f9e51d146eae5
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-27 00:23:11 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,12 +5,10 @@ attr_accessor :tester
   def initialize(name)
     @tester = name
     @tester
-
-    binding.pry
   end
 
   def match(array)
-  a = @tester.select do |x| x.split("").sort == array.split("").sort
+  @tester.select do |x| x.split("").sort == array.split("").sort
 binding.pry
   end
   a
